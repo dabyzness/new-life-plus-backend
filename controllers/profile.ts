3419920ef99389870bd881d5d4d647602b0aaaee
Damian <bzdyrad@gmail.com>
2023-02-23 17:27:54 -0500

    implement getAllTasks route and function for a profile

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -42,4 +42,70 @@ async function getProfile(req: Request, res: Response) {
   }
 }
 
-export { createProfile as create, getProfile };
+async function getTasks(req: Request, res: Response) {
+  try {
+    const { username } = req.params;
+    if (!username) {
+      throw new Error("No username provided.");
+    }
+
+    if (username !== req.token?.username) {
+      throw new Error(
+        "Permission denied. Cannot access tasks of another user."
+      );
+    }
+
+    const profile = await prisma.profile.findFirst({ where: { username } });
+
+    if (!profile) {
+      throw new Error("Profile does not exist.");
+    }
+
+    // Shortern this function. Create a helper function for queries so that I don't need these long ugly queries
+    const tasks: Task[] = await Promise.all([
+      prisma.task.findMany({
+        where: { profile_id: profile.id, frequency_type: "DAILY" },
+        select: {
+          id: true,
+          name: true,
+          skill: true,
+          frequency_type: true,
+          streak: true,
+          num_completed: true,
+          daily_freq: true,
+        },
+      }),
+      prisma.task.findMany({
+        where: { profile_id: profile.id, frequency_type: "WEEKLY" },
+        select: {
+          id: true,
+          name: true,
+          skill: true,
+          frequency_type: true,
+          streak: true,
+          num_completed: true,
+          weekly_freq: true,
+        },
+      }),
+      prisma.task.findMany({
+        where: { profile_id: profile.id, frequency_type: "MONTHLY" },
+        select: {
+          id: true,
+          name: true,
+          skill: true,
+          frequency_type: true,
+          streak: true,
+          num_completed: true,
+        },
+      }),
+    ]).then((value) => value.flat());
+
+    console.log(`Test: `, tasks);
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+export { createProfile as create, getProfile, getTasks };
--- routes/api/profile.ts
+++ routes/api/profile.ts
@@ -5,6 +5,7 @@ import { auth } from "../../middlewares/auth";
 const router = Router();
 
 router.get("/:username", auth, profileController.getProfile);
+router.get("/:username/tasks", auth, profileController.getTasks);
 router.post("/", auth, profileController.create);
 
 export { router };
